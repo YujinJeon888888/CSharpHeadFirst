16b8c6ce1920da290ac083e816cafccbc5aa2048
YujinJeon888888 <yujinjun@ewhain.net>
2023-10-04 16:30:13 +0900

    .

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,16 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("하이욤", "메세지박스의 제목");
+
+        }
+    }
+}
